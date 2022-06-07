29cd48f06ff847975c146bc1960eea48a2b97fd3
GABRIEL  BRAZ <gabrielbrazdev@gmail.com>
2022-06-07 09:39:23 -0300

    post

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
-
+const fs = require('fs');
+const clientes = require("./clientes.json");
 const app = express();
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    return res.json({titulo: "Como criar API"});
+    return res.json({titulo: "API ok!"});
 });
+
+app.get("/clientes", (req, res) => {
+    return res.json(clientes);
+});
+
+app.post("/clientes", (req, res) => {
+    const { nome, idade, email, senha } = req.body;
+    fs.writeFileSync("clientes.json",  JSON.stringify(req.body))
+    console.log(req.body);
+    return res.json(clientes);
+});
+
 
 app.listen(8080, () =>{
     console.log("Servidor iniciado na porta 8080: http://localhost:8080/");
---
+++ cliente.model.js
@@ -0,0 +1,8 @@
+class Customer  {
+    nome = "";
+    idade = 0;
+    email= "";
+    senha= ""
+  };
+
+ export default Customer;
